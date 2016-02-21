0395431366506bfb08df145d9fc408f568141e86
Sebastiano <olivo.sebastiano@gmail.com>
2016-02-21 15:43:12 +0100

    create a method for colission and for a class for keyboard event

---
+++ src/scripts/Keyboard.js
@@ -0,0 +1,70 @@
+function Keyboard(keyCode) {
+
+    this.code = keyCode;
+    this.isDown = false;
+    this.isUp = true;
+    this.press = undefined;
+    this.release = undefined;
+    return this;
+
+}
+
+Keyboard.prototype.downHandler = function(event) {
+    if (event.keyCode === this.code) {
+        if (this.isUp && this.press) this.press();
+        this.isDown = true;
+        this.isUp = false;
+    }
+    event.preventDefault();
+};
+
+
+Keyboard.prototype.upHandler = function(event) {
+    if (event.keyCode === this.code) {
+        if (this.isDown && this.release) this.release();
+        this.isDown = false;
+        this.isUp = true;
+    }
+    event.preventDefault();
+};
+
+
+window.addEventListener(
+    "keydown", this.downHandler.bind(this), false
+);
+window.addEventListener(
+    "keyup", this.upHandler.bind(this), false
+);
+
+
+/*
+##Use
+
+  var left = new Keyboard(37),
+      right = new Keyboard(39);
+
+
+  left.press = function() {
+    elemPosition.x  = -5;
+  };
+
+  left.release = function() {
+
+    if (!right.isDown && cat.vy === 0) {
+      elemPosition.x = 0;
+    }
+  };
+
+  right.press = function() {
+    elemPosition.x  = +5;
+  };
+
+  right.release = function() {
+
+    if (!right.isDown && cat.vy === 0) {
+      elemPosition.x = 0;
+    }
+  };
+
+
+*/
---
+++ src/scripts/hitTest.js
@@ -0,0 +1,51 @@
+MapGenerator.prototype.hitTest = function(elem1, elem1X, elem1Y, elem2) {
+
+    var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+    //hit will determine whether there's a collision
+    hit = false;
+
+    //Find the center points of each sprite
+    elem1.centerX = elem1X + elem1.width / 2;
+    elem1.centerY = elem1Y.y + elem1.height / 2;
+    elem2.centerX = elem2.x + elem2.width / 2;
+    elem2.centerY = elem2.y + elem2.height / 2;
+
+    //Find the half-widths and half-heights of each sprite
+    elem1.halfWidth = elem1.width / 2;
+    elem1.halfHeight = elem1.height / 2;
+    elem2.halfWidth = elem2.width / 2;
+    elem2.halfHeight = elem2.height / 2;
+
+    //Calculate the distance vector between the sprites
+    vx = elem1.centerX - elem2.centerX;
+    vy = elem1.centerY - elem2.centerY;
+
+    //Figure out the combined half-widths and half-heights
+    combinedHalfWidths = elem1.halfWidth + elem2.halfWidth;
+    combinedHalfHeights = elem1.halfHeight + elem2.halfHeight;
+
+    //Check for a collision on the x axis
+    if (Math.abs(vx) < combinedHalfWidths) {
+
+        //A collision might be occuring. Check for a collision on the y axis
+        if (Math.abs(vy) < combinedHalfHeights) {
+
+            //There's definitely a collision happening
+            hit = true;
+        } else {
+
+            //There's no collision on the y axis
+            hit = false;
+        }
+    } else {
+
+        //There's no collision on the x axis
+        hit = false;
+    }
+
+    //`hit` will be either `true` or `false`
+    return hit;
+
+
+};
